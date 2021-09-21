01e91b6dd2ba8f8f40133dc423335d45d8dee869
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2021-09-21 15:38:15 -0300

    Update TurDSMTest.java

--- src/main/java/com/viglet/turing/wem/dsm/TurDSMTest.java
+++ src/main/java/com/viglet/turing/wem/dsm/TurDSMTest.java
@@ -19,15 +19,15 @@ import com.viglet.turing.client.sn.response.QueryTurSNResponse;
 
 public class TurDSMTest {
 	private static Logger logger = Logger.getLogger(TurSNServer.class.getName());
+
 	public TurDSMTest() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		/*
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-		System.out.println(sdf.format(new Date()));
+		 * SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+		 * System.out.println(sdf.format(new Date()));
 		 */
 
 		HttpTurSNServer turSNServer = null;
@@ -38,10 +38,10 @@ public class TurDSMTest {
 		}
 		TurSNQuery query = new TurSNQuery();
 		ArrayList<String> facets = new ArrayList<>();
-		
+
 		facets.add("host:enwikipediaorg");
 		facets.add("type:Page");
-		
+
 		query.setQuery("*");
 		query.setFieldQueries(facets);
 		query.setRows(10);
@@ -51,41 +51,38 @@ public class TurDSMTest {
 		QueryTurSNResponse response = turSNServer.query(query);
 		TurSNDocumentList turSNResults = response.getResults();
 		TurSNPagination turSNPagination = response.getPagination();
-		
+
 		TurSNFacetFieldList facetFields1 = response.getFacetFields();
-		//facetFields1.getTurSNFacetFields()
+		// facetFields1.getTurSNFacetFields()
 		response.getFacetFields().getFacetWithRemovedValues().ifPresent(facetToRemove -> {
 			System.out.println("---");
 			System.out.println(facetToRemove.getLabel());
 			facetToRemove.getValues().forEach(value -> {
 				System.out.println(value.getLabel());
-				value.getQueryParams().get().entrySet().forEach(queryParam ->{
+				value.getQueryParams().get().entrySet().forEach(queryParam -> {
 					System.out.println(queryParam.getKey() + " = " + queryParam.getValue());
 				});
 			});
 		});
 		response.getFacetFields().forEach(facetFields -> {
-			//facetFields.
+			// facetFields.
 			System.out.println("Facet: " + facetFields.getLabel());
-			facetFields.getValues().forEach(
-					facetField -> System.out.println(facetField.getLabel() + facetField.getApiURL()  + "(" + facetField.getCount() + ")"));
+			facetFields.getValues().forEach(facetField -> System.out
+					.println(facetField.getLabel() + facetField.getApiURL() + "(" + facetField.getCount() + ")"));
 		});
 		System.out.println("");
 		System.out.println("---");
-		
-		
+
 		turSNPagination.getAllPages().forEach(page -> {
 			System.out.print(page.getLabel() + "-");
 			System.out.println(page.getPageNumber());
-			//Object obj = page.getQueryParams().isPresent()
-			if (page.getQueryParams().isPresent())
-			{
-				page.getQueryParams().get().entrySet().forEach(queryParam ->{
-					System.out.print(queryParam.getKey() + " = " );
+			// Object obj = page.getQueryParams().isPresent()
+			if (page.getQueryParams().isPresent()) {
+				page.getQueryParams().get().entrySet().forEach(queryParam -> {
+					System.out.print(queryParam.getKey() + " = ");
 					System.out.println(queryParam.getValue());
 				});
-			}
-			else
+			} else
 				System.out.println("------------------Ruim---------------------------------------------");
 		});
 		System.out.println("");
@@ -107,61 +104,47 @@ public class TurDSMTest {
 
 		System.out.println(turSNPagination.getPageNumberList());
 
-
 		List<TurSNPaginationItem> pages = turSNPagination.getAllPages();
-		if (pages.size() > 1) 
-		{
-			
-			for (TurSNPaginationItem page : pages)
-			{
-				String linkLabel = page.getType().equals(TurSNPaginationType.PAGE)? Integer.toString(page.getPageNumber()): page.getLabel();
-				if (page.getType().equals(TurSNPaginationType.PAGE))
-					System.out.println("<a href=\"" + "?text1=" + "*" + "&page1=" + page.getPageNumber() + "\">" + page.getType().equals(TurSNPaginationType.PAGE)? page.getPageNumber(): page.getLabel() + "</a>");
-				else
-					System.out.println("<a href=\"" + "?text1=" + "*" + "&page1=" + page.getPageNumber() + "\">" + page.getLabel() + "</a>");
+		if (pages.size() > 1) {
+
+			for (TurSNPaginationItem page : pages) {
+				String linkLabel = page.getType().equals(TurSNPaginationType.PAGE)
+						? Integer.toString(page.getPageNumber())
+						: page.getLabel();
+				System.out.println(
+						"<a href=\"" + "?text1=" + "*" + "&page1=" + page.getPageNumber() + "\">" + linkLabel + "</a>");
 			}
-		}
-		else
-		{
+		} else {
 			System.out.println("Only 1 page");
 		}
 
-
+		/*
+		 * TurSNQuery query = new TurSNQuery(); query.setQuery("*"); query.setRows(10);
+		 * query.setSortField(TurSNQuery.ORDER.asc); query.setPageNumber(1);
+		 * 
+		 * QueryTurSNResponse response = turSNServer.query(query); TurSNDocumentList
+		 * turSNResults = response.getResults(); TurSNPagination turSNPagination =
+		 * response.getPagination();
+		 * System.out.println(turSNPagination.getAllPages().size());
+		 */
+		// turSNPagination.getAllPages().forEach(page -> {
+		// System.out.println(page.getLabel());
+		// });
+		//
+		// //System.out.println(turSNPagination.getCurrentPage().getPageNumber());
+		// System.out.println(turSNPagination.getPageNumberList());
+		// System.out.println(turSNPagination.getLastPage());
 
 		/*
-		TurSNQuery query = new TurSNQuery();
-		query.setQuery("*");
-		query.setRows(10);
-		query.setSortField(TurSNQuery.ORDER.asc);
-		query.setPageNumber(1);
-
-		QueryTurSNResponse response = turSNServer.query(query);
-		TurSNDocumentList turSNResults = response.getResults();
-		TurSNPagination turSNPagination = response.getPagination();
-		System.out.println(turSNPagination.getAllPages().size());
-		 */		
-		//		turSNPagination.getAllPages().forEach(page -> {
-		//			System.out.println(page.getLabel());
-		//		});
-		//		
-		//		//System.out.println(turSNPagination.getCurrentPage().getPageNumber());
-		//		System.out.println(turSNPagination.getPageNumberList());
-		//		System.out.println(turSNPagination.getLastPage());
-
-		/*		
-		for (TurSNDocument document : turSNResults.getTurSNDocuments())
-		{
-			Map doc = document.getContent().getFields();
-			for (Object keyObj : doc.keySet())
-			{
-				String key = (String) keyObj;
-				//String value = (doc.get(key) != null) ? (String) doc.get(key) : "";
-				System.out.println("<BR>" + key + " = " + doc.get(key));
-
-
-			}
-		}
-
+		 * for (TurSNDocument document : turSNResults.getTurSNDocuments()) { Map doc =
+		 * document.getContent().getFields(); for (Object keyObj : doc.keySet()) {
+		 * String key = (String) keyObj; //String value = (doc.get(key) != null) ?
+		 * (String) doc.get(key) : ""; System.out.println("<BR>" + key + " = " +
+		 * doc.get(key));
+		 * 
+		 * 
+		 * } }
+		 * 
 		 */
 	}
 
